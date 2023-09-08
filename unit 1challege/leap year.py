4aa606cdc76dec7336c42c63e2af49f678c59057
deva-2005 <144435086+deva-2005@users.noreply.github.com>
2023-09-08 14:49:32 +0530

    Add files via upload

---
+++ unit 1challege/leap year.py
@@ -0,0 +1,37 @@
+def isleapyear(year):
+  if(year%400==0):
+    return True
+  else:
+    return False
+year=int(input("Enter a Number:"))
+if isleapyear(year):
+  print("{} is not a leap year.".format(year))
+else:def isleapyear(year):
+  if(year%400==0):
+    return True
+  else:
+    return False
+year=int(input("Enter a Number:"))
+if isleapyear(year):def isleapyear(year):
+  if(year%400==0):
+    return True
+  else:
+    return False
+year=int(input("Enter a Number:"))
+if isleapyear(year):
+  print("{} is not a leap year.".format(year))
+else:
+  print("{} is a leap year.".format(year))
+  print("{} is not a leap year.".format(year))
+else:
+  print("{} is a leap year.".format(year))
+  print("{} is a leap year.".format(year))def isleapyear(year):
+  if(year%400==0):
+    return True
+  else:
+    return False
+year=int(input("Enter a Number:"))
+if isleapyear(year):
+  print("{} is not a leap year.".format(year))
+else:
+  print("{} is a leap year.".format(year))
